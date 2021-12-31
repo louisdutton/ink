a1ea36853d62030d9f150e7978b2ea0caa9691c3
Louis Dutton <clouisdutton@gmail.com>
2021-12-31 12:53:36 +0000

    converted SocketContext to React.FC

--- client/components/SocketContext.tsx
+++ client/components/SocketContext.tsx
@@ -1,13 +1,14 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, FC } from 'react';
 import io, { Socket } from 'socket.io-client';
 
 const socket = io('https://localhost:3000');
 const SocketContext = createContext<Socket>(socket);
 
-export default function SocketProvider(props: any) {
-	return (
-		<SocketContext.Provider value={socket}>{...props}</SocketContext.Provider>
-	);
-}
+const SocketProvider: FC = (props) => (
+	<SocketContext.Provider value={socket}>
+		{props.children}
+	</SocketContext.Provider>
+);
 
+export default SocketProvider;
 export const useSockets = () => useContext(SocketContext);
