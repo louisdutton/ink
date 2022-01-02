d2d342caa68d8bb732f68a39f5a800f2d03e4065
Louis Dutton <clouisdutton@gmail.com>
2022-01-02 16:49:07 +0000

    added production config

--- config/default.ts
+++ config/default.ts
@@ -1,1 +1,4 @@
-export const SOCKET_URL = 'http://localhost:4000';
+export const SOCKET_URL =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:4000'
+		: 'https://draw-dot-ink.herokuapp.com:4000';
