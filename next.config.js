5ba5393e5c1a03ea8ac284c1f00fea74d5d88c66
Louis Dutton <clouisdutton@gmail.com>
2022-01-08 20:58:04 +0000

    nextjs wasm path build workaround

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	webpack: function (config, options) {
-		// config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+	webpack: function (config, { isServer, dev }) {
 		// Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
 		config.experiments = { syncWebAssembly: true, topLevelAwait: true };
+
+		// In prod mode and in the server bundle (the place where this "chunks" bug
+		// appears), use the client static directory for the same .wasm bundle
+		config.output.webassemblyModuleFilename =
+			isServer && !dev ? '../static/wasm/[id].wasm' : 'static/wasm/[id].wasm';
+
+		// Ensure the filename for the .wasm bundle is the same on both the client
+		// and the server (as in any other mode the ID's won't match)
+		config.optimization.moduleIds = 'named';
+
 		return config;
 	}
 };
