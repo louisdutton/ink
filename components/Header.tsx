3a091f6f346a6982c9cc2a69c139be1b8bf17aa2
Louis Dutton <clouisdutton@gmail.com>
2022-01-22 20:53:35 +0000

    hide header for non-auth user

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Navigation from "./Navigation";
 import Identity from "./Identity";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebase";
 
 export default function Header() {
-	// if (!username) return <div />;
+	const [user, loading, error] = useAuthState(auth);
+
+	if (!user) return <div />;
 	return (
 		<header className="absolute top-0 left-0 z-50 flex justify-between w-screen px-4 py-3">
 			<Identity />
